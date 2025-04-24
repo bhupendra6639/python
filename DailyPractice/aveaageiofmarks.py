ee2c161f191e21326dbaa08bafce5bb5c9e712cc
bhupendra6639 <bhupendrpatil6639@gmail.com>
2025-04-24 16:36:14 +0530

    python assignment

---
+++ DailyPractice/aveaageiofmarks.py
@@ -0,0 +1,6 @@
+marks = [85, 90, 78, 88, 92]
+
+for i in marks:
+       sum=sum+i
+sum=sum(marks)
+#print(sum/len(marks))
---
+++ DailyPractice/listtype.py
@@ -0,0 +1,3 @@
+items = [10, 'hello', True]
+for i in items:
+     print(type(i))
